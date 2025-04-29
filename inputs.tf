4f71f8f9dbe1638c52b5aac9386dd6b07b23a055
Kulbhushan <kulbhushan.mayer@gmail.com>
2025-04-29 09:50:09 +0530

    Update type for tags to map(string)

--- inputs.tf
+++ inputs.tf
@@ -6,7 +6,7 @@ variable "control_plane" {
     associate_public_ip_address = optional(bool, true)
     vpc_security_group_ids      = list(string)
     subnet_id                   = string
-    tags                        = string
+    tags                        = map(string)
     user_data                   = string
     private_key_file            = string
     private_key                 = string
